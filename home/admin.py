cc697506af97246c5b4ee9d0158c47cf13dabf5b
Crowdbotics <engineering@crowdbotics.com>
2022-10-26 08:28:26 +0000

    Update models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Asdfd
+admin.site.register(Asdfd)
 
 # Register your models here.
--- home/models.py
+++ home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Asdfd(models.Model):
+    'Generated Model'
+    bvbvvn = models.BigIntegerField()
